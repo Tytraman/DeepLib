e04dc757f268c4ab9ae952b9fd08e144fe115028
Tytraman <tytraman@gmail.com>
2025-12-03 20:42:03 +0100

    add bitmask

---
+++ DeepLib/DeepLib/utils/bitmask.hpp
@@ -0,0 +1,171 @@
+﻿#ifndef DEEP_LIB_BITMASK_HPP
+#define DEEP_LIB_BITMASK_HPP
+
+#include "DeepLib/deep_lib_export.h"
+#include "DeepCore/types.hpp"
+
+namespace deep
+{
+    template <typename Type>
+    class bitmask
+    {
+        static_assert(is_integral<Type>, "Type must be integral");
+
+      public:
+        constexpr bitmask() noexcept;
+        constexpr bitmask(Type value) noexcept;
+
+        /**
+         * @brief Active le bit en mettant sa valeur à 1.
+         * @param position La position du bit à activer.
+         */
+        constexpr void set(uint8 position) noexcept;
+
+        /**
+         * @brief Désactive le bit en mettant sa valeur à 0.
+         * @param position La position du bit à désactiver.
+         */
+        constexpr void reset(uint8 position) noexcept;
+
+        /**
+         * @brief Inverse la valeur du bit.
+         * @param position Le position du bit à inverser.
+         */
+        constexpr void flip(uint8 position) noexcept;
+
+        /**
+         * @brief Vérifie si le bit est activé.
+         * @param position La position du bit à vérifier.
+         * @return true si activé.
+         */
+        constexpr bool test(uint8 position) noexcept;
+
+        constexpr bitmask<Type> operator&(const bitmask<Type> &other) const noexcept;
+        constexpr bitmask<Type> &operator&=(const bitmask<Type> &other) noexcept;
+
+        constexpr bitmask<Type> operator|(const bitmask<Type> &other) const noexcept;
+        constexpr bitmask<Type> operator|=(const bitmask<Type> &other) noexcept;
+
+        constexpr bitmask<Type> operator^(const bitmask<Type> &other) const noexcept;
+        constexpr bitmask<Type> operator^=(const bitmask<Type> &other) noexcept;
+
+        constexpr bitmask<Type> operator~() const noexcept;
+
+        constexpr bool operator==(const bitmask<Type> &other) const noexcept;
+        constexpr bool operator!=(const bitmask<Type> &other) const noexcept;
+
+        constexpr Type get_bits() const noexcept;
+
+      private:
+        Type m_bits;
+    };
+
+    template <typename Type>
+    inline constexpr bitmask<Type>::bitmask() noexcept
+            : m_bits(static_cast<Type>(0))
+    {
+    }
+
+    template <typename Type>
+    inline constexpr bitmask<Type>::bitmask(Type value) noexcept
+            : m_bits(value)
+    {
+    }
+
+    template <typename Type>
+    inline constexpr void bitmask<Type>::set(uint8 position) noexcept
+    {
+        m_bits |= (static_cast<Type>(1) << position);
+    }
+
+    template <typename Type>
+    inline constexpr void bitmask<Type>::reset(uint8 position) noexcept
+    {
+        m_bits &= ~(static_cast<Type>(1) << position);
+    }
+
+    template <typename Type>
+    inline constexpr void bitmask<Type>::flip(uint8 position) noexcept
+    {
+        m_bits ^= (static_cast<Type>(1) << position);
+    }
+
+    template <typename Type>
+    inline constexpr bool bitmask<Type>::test(uint8 position) noexcept
+    {
+        return (m_bits & (static_cast<Type>(1) << position)) != 0;
+    }
+
+    template <typename Type>
+    inline constexpr bitmask<Type> bitmask<Type>::operator&(const bitmask<Type> &other) const noexcept
+    {
+        return bitmask<Type>(m_bits & other.m_bits);
+    }
+
+    template <typename Type>
+    inline constexpr bitmask<Type> &bitmask<Type>::operator&=(const bitmask<Type> &other) noexcept
+    {
+        m_bits &= other.m_bits;
+
+        return *this;
+    }
+
+    template <typename Type>
+    inline constexpr bitmask<Type> bitmask<Type>::operator|(const bitmask<Type> &other) const noexcept
+    {
+        return bitmask<Type>(m_bits | other.m_bits);
+    }
+
+    template <typename Type>
+    inline constexpr bitmask<Type> bitmask<Type>::operator|=(const bitmask<Type> &other) noexcept
+    {
+        m_bits |= other.m_bits;
+
+        return *this;
+    }
+
+    template <typename Type>
+    inline constexpr bitmask<Type> bitmask<Type>::operator^(const bitmask<Type> &other) const noexcept
+    {
+        return bitmask<Type>(m_bits ^ other.m_bits);
+    }
+
+    template <typename Type>
+    inline constexpr bitmask<Type> bitmask<Type>::operator^=(const bitmask<Type> &other) noexcept
+    {
+        m_bits ^= other.m_bits;
+
+        return *this;
+    }
+
+    template <typename Type>
+    inline constexpr bitmask<Type> bitmask<Type>::operator~() const noexcept
+    {
+        return bitmask<Type>(~m_bits);
+    }
+
+    template <typename Type>
+    inline constexpr bool bitmask<Type>::operator==(const bitmask<Type> &other) const noexcept
+    {
+        return m_bits == other.m_bits;
+    }
+
+    template <typename Type>
+    inline constexpr bool bitmask<Type>::operator!=(const bitmask<Type> &other) const noexcept
+    {
+        return m_bits != other.m_bits;
+    }
+
+    template <typename Type>
+    inline constexpr Type bitmask<Type>::get_bits() const noexcept
+    {
+        return m_bits;
+    }
+
+    using bitmask8  = bitmask<uint8>;
+    using bitmask16 = bitmask<uint16>;
+    using bitmask32 = bitmask<uint32>;
+    using bitmask64 = bitmask<uint64>;
+} // namespace deep
+
+#endif
---
+++ Test/utils/test_bitmask.cpp
@@ -0,0 +1,177 @@
+﻿#include "DeepLib/lib.hpp"
+#include "DeepLib/utils/bitmask.hpp"
+
+int main(int argc, char *argv[])
+{
+    deep::ctx *context = deep::lib::create_ctx();
+
+    if (context == nullptr)
+    {
+        return 1;
+    }
+
+    deep::bitmask16 bits;
+    if (bits.get_bits() != 0)
+    {
+        return 10;
+    }
+
+    deep::bitmask16 bits_A7 = 0xA7;
+    if (bits_A7.get_bits() != 0xA7)
+    {
+        return 11;
+    }
+
+    // --- TEST de set() ---
+
+    bits.set(0);
+    if (!bits.test(0))
+    {
+        return 12;
+    }
+
+    if (bits.get_bits() != 1)
+    {
+        return 13;
+    }
+
+    bits.set(15);
+    if (!bits.test(15))
+    {
+        return 14;
+    }
+    if (!bits.test(0))
+    {
+        return 15;
+    }
+
+    // --- TEST de reset() ---
+
+    bits.reset(0);
+    if (bits.test(0))
+    {
+        return 20;
+    }
+
+    if (!bits.test(15))
+    {
+        return 21;
+    }
+
+    bits.reset(15);
+    if (bits.test(15))
+    {
+        return 22;
+    }
+
+    // --- TEST de flip() ---
+
+    bits.set(15);
+
+    bits.flip(15);
+
+    if (bits.test(15))
+    {
+        return 30;
+    }
+
+    bits.flip(2);
+
+    if (!bits.test(2))
+    {
+        return 31;
+    }
+
+    // --- TEST des opérateurs binaires ---
+
+    deep::bitmask16 m1 = 0x3;
+    deep::bitmask16 m2 = 0x5;
+
+    deep::bitmask16 result_or = m1 | m2;
+
+    if (result_or.get_bits() != 0x7)
+    {
+        return 40;
+    }
+
+    deep::bitmask16 result_and = m1 & m2;
+
+    if (result_and.get_bits() != 0x1)
+    {
+        return 41;
+    }
+
+    deep::bitmask16 result_xor = m1 ^ m2;
+
+    if (result_xor.get_bits() != 0x6)
+    {
+        return 42;
+    }
+
+    // --- TEST des opérateurs d'assignation ---
+
+    deep::bitmask16 assign = 0x1;
+
+    assign |= 0x2;
+
+    if (assign.get_bits() != 0x3)
+    {
+        return 50;
+    }
+
+    assign &= 0x1;
+
+    if (assign.get_bits() != 0x1)
+    {
+        return 51;
+    }
+
+    assign ^= 0x1;
+
+    if (assign.get_bits() != 0x0)
+    {
+        return 52;
+    }
+
+    // --- TEST de l'opérateur NOT ---
+
+    deep::bitmask16 zero;
+    if (zero != 0)
+    {
+        return 60;
+    }
+
+    deep::bitmask16 inverted = ~zero;
+    if (inverted.get_bits() != 0xFFFF)
+    {
+        return 61;
+    }
+
+    // --- TEST des comparaisons ---
+
+    deep::bitmask16 cmp1 = 100;
+    deep::bitmask16 cmp2 = 100;
+    deep::bitmask16 cmp3 = 666;
+
+    if (!(cmp1 == cmp2))
+    {
+        return 70;
+    }
+
+    if (cmp1 != cmp2)
+    {
+        return 71;
+    }
+
+    if (cmp1 == cmp3)
+    {
+        return 72;
+    }
+
+    if (!deep::lib::destroy_ctx(context))
+    {
+        return 100;
+    }
+
+    return 0;
+}
